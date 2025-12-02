86835be547947b8b929607cbb3052a390ab34b0e
vegabook <thomas@scendance.fr>
2025-12-02 21:55:11 +0000

    first zig commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Python 3.11 development environment";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          # The Nix packages provided in the environment
+          packages = with pkgs; [
+            python312
+            python312Packages.uv
+            sqlite
+          ];
+
+          shellHook = ''
+            if [ -z "$NIX_SHELL_NESTED" ]; then
+              export NIX_SHELL_NESTED=1
+              alias ipy="uv run ipython --nosep"
+              export PS1="⚡ \e[38;5;208m\][Z]e[38;5;015m\][ZIG\e[0m $PS1"
+            else
+              echo "Nested nix-shell detected, skipping uv init"
+              export PS1="⚡ \e[38;5;208m\][Z]e[38;5;015m\][ZIG\e[0m [NESTED] $PS1"
+            fi
+          '';
+        };
+      });
+    };
+}
+
+
